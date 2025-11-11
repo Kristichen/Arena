8a44fcd4b272370802943e660b371aba8c98c63c
Kristichen <chenkhrystyna@gmail.com>
2025-11-11 13:50:29 +0100

    jumping

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from constants import *
 
 import random
 
+from player import Player
 # pygame setup
 pygame.init()
 
@@ -12,6 +13,7 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Fate of Nature: Survive the Arena")
 clock = pygame.time.Clock()
 running = True
+player=Player(100,100,4,4)
 
 while running:
     # poll for events
@@ -20,7 +22,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    pressed = pygame.key.get_pressed()
     screen.fill("black")
+    player.update(pressed)
+    player.draw(screen)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
